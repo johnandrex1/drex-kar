f3d02e454f84ea10a54d6baa60a6455f6ef7a978
Andrex <johnandrex1@gmail.com>
2026-02-08 11:03:19 +0800

    Updates

---
+++ _next/static/WMqmgY3599opkVg-3rxDD/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/main//_next/:path+",
+        "destination": "/main/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
